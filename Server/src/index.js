171dde2f1d3b44ba9615a3df6e23287eeabbc4d4
valenpe <valentinaperez230701@gmail.com>
2023-09-21 15:25:42 -0300

    llegando al final

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,10 +1,14 @@
 const { server } = require("./app")
+const { conn } = require('./DB_connection');
 const PORT = 3001;
 
-server.listen(PORT, () => {
-  console.log(`Server raised in port: ${PORT}`);
-})
 
+conn.sync({force: true}).then(()=>{
+
+  server.listen(PORT, () => {
+    console.log(`Server raised in port: ${PORT}`);
+  })
+  })
 
 
 
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -10,6 +10,7 @@ router.post('/fav', postFav)
 router.delete('/fav/:id', deleteFav)
 
 
+
 module.exports= router;
 
 
